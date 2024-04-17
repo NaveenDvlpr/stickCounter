5152dd9e3864828f80c48a2280e5c84d4d84989a
NaveenDvlpr <naveenmoyya01@gmail.com>
2024-04-17 15:07:37 +0530

    load and restart

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const app = document.querySelector(".app");
 const container = document.querySelector(".container");
+const hot100 = document.querySelector(".hot100");
+const infinite = document.querySelector(".infinite");
+const challenge = document.querySelector(".challenge");
+const restart = document.querySelector(".restart");
+const score = document.querySelector(".score");
 
 
-let count = 0;
+
+let count;
 
 const addCounter = () => {
     const counter = document.createElement("div");
@@ -20,20 +26,23 @@ const disableCounter = (counter) => {
 const addSticks = (e) => {
     if(e.target.matches(".counter")) {
         count++;
-        localStorage.setItem("count", JSON.stringify(count)); 
+        localStorage.setItem("count", count); 
         const stick = document.createElement("div");
         stick.classList.add("stick");
         e.target.appendChild(stick);
     }
 };
 
-const handleCounter = (e) => {
-    if(count === 0) {
-        addCounter();
-        return;
+const addPlaneSticks = (num, counter) => {
+    for(let i = 1; i <= num; i++) {
+        const stick = document.createElement("div");
+        stick.classList.add("stick");
+        counter.appendChild(stick);
     }
+}
+
+const handleCounter = (e) => {
     if(count%5 === 0 && e.target.matches(".counter")) {
-        console.log(count);
         const counters = document.getElementsByClassName("counter");
         console.log(counters);
         if(counters.length > 0) disableCounter(counters[counters.length-1]);
@@ -42,12 +51,34 @@ const handleCounter = (e) => {
     
 }
 
+const handleLoad = () => {
+    count = localStorage.getItem("count");
+    console.log(count);
+    for(let i = count; i >= 0; i-=5) {
+        const counter = document.createElement("div");
+        if(i < 5) {
+            counter.classList.add("counter");
+            counter.addEventListener("click", addSticks);
+            addPlaneSticks(i, counter);
+        } else {
+            counter.classList.add("oldCounter");
+            addPlaneSticks(5, counter);
+        }
+        container.insertBefore(counter, container.firstChild);
+    }
+    
+}
+
+const handleRestart = () => {
+    container.innerHTML = "";
+    addCounter();
+    count = 0;
+    localStorage.setItem("count", count);
+}
 
 
 
-
+window.addEventListener("load", handleLoad);
 container.addEventListener("click", handleCounter);
-window.addEventListener("load", handleCounter);
+restart.addEventListener("click", handleRestart);
 
-
-console.log(container);
